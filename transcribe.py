241959963380c8bdf8f460677b0e7d59ebdd2337
Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2023-07-26 14:47:41 +0800

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,10 +1,11 @@
-#pip install SpeechRecognition reportlab
-
+import streamlit as st
+import os
+import shutil
 import speech_recognition as sr
 from reportlab.lib.pagesizes import letter
-from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import SimpleDocTemplate, Paragraph
+import base64
 
 def transcribe_audio(audio_file_path):
     recognizer = sr.Recognizer()
@@ -23,17 +24,50 @@ def transcribe_audio(audio_file_path):
 def save_to_pdf(transcribed_text, output_file_path):
     doc = SimpleDocTemplate(output_file_path, pagesize=letter)
     styles = getSampleStyleSheet()
-    content = [Paragraph(transcribed_text, styles["Normal"])]
+    content = [Paragraph(text, styles["Normal"]) for text in transcribed_text.split("\n") if text.strip()]
     doc.build(content)
 
+def get_pdf_download_link(file_path):
+    with open(file_path, "rb") as file:
+        pdf_bytes = file.read()
+        b64 = base64.b64encode(pdf_bytes).decode()
+        href = f"<a href='data:application/octet-stream;base64,{b64}' download='transcription.pdf'>Download PDF</a>"
+        return href
+
+def main():
+    st.title("Audio Transcription and PDF Generation")
+
+    uploaded_file = st.file_uploader("Upload an audio file", type=["wav", "mp3"])
+
+    if uploaded_file:
+        # Create a temporary directory to save the uploaded audio file
+        temp_directory = "temp_audio"
+        os.makedirs(temp_directory, exist_ok=True)
+
+        # Save the uploaded audio file to the temporary directory
+        audio_file_path = os.path.join(temp_directory, uploaded_file.name)
+        with open(audio_file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+
+        st.audio(uploaded_file)
+
+        if st.button("Generate PDF"):
+            # Transcribe the audio
+            result = transcribe_audio(audio_file_path)
+
+            # Create a directory to save the generated PDF
+            pdf_directory = "output_pdf"
+            os.makedirs(pdf_directory, exist_ok=True)
+
+            # Generate the PDF file
+            output_file_path = os.path.join(pdf_directory, "transcription.pdf")
+            save_to_pdf(result, output_file_path)
+
+            st.success("Transcription and PDF generation completed successfully!")
+            st.markdown(get_pdf_download_link(output_file_path), unsafe_allow_html=True)
+
+            # Remove the temporary audio directory after processing
+            shutil.rmtree(temp_directory)
+
 if __name__ == "__main__":
-    audio_file_path = "path_to_audio_file.wav"  # Replace with the path to your audio file
-    output_file_path = "output_transcription.pdf"  # Replace with your desired output PDF file path
-
-    result = transcribe_audio(audio_file_path)
-    print("Transcription Result:")
-    print(result)
-
-    # Save the transcription to a PDF file
-    save_to_pdf(result, output_file_path)
-    print(f"Transcription saved to {output_file_path}")
+    main()
